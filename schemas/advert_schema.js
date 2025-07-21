600b768e1db870b4921c657d331bed27881c5830
kofi-lartey <kofilartey12@gmail,com>
2025-07-21 15:20:12 +0000

    updated the advert model and schema

--- schemas/advert_schema.js
+++ schemas/advert_schema.js
@@ -1,16 +1,18 @@
-import Joi from 'joi'
+import Joi from 'joi';
 
 export const advertSchema = Joi.object({
-    name: Joi.string().required(),
-    description: Joi.string().required(),
-    price: Joi.number().required(),
-    // sizes: Joi.array()
-    //     .items(Joi.string().valid('S', 'M', 'L', 'XL', 'XXL'))
-    //     .min(1)
-    //     .required(),
-    // category: Joi.string().valid('matchingSet', 'top', 'kids', 'beauty','curve','men','women').required(),
-    // subCategory: Joi.string().valid('topwear', 'bottomwear', 'dresses', 'jewellery', 'cosmetics').required(),
-    images: Joi.array().items(Joi.string()).min(1).required(),
-    bestSeller: Joi.boolean(),
-    date: Joi.date(),
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  price: Joi.number().required(),
+  images: Joi.array().items(Joi.string()).min(1).required(),
+  category: Joi.string().required(),
+  unit: Joi.string().required(),
+  farmer: Joi.string().required(),
+  location: Joi.string().required(),
+  rating: Joi.string().required(),
+  reviews: Joi.string().required(),
+  freshness: Joi.string().required(),
+  quantity: Joi.string().required(),
+  bestseller: Joi.boolean(),
+  date: Joi.date().required()
 });
